9a2959ed30eda8a077bb955cb8e91eeadcd0e896
HarveyMGJ <hgonzalezjimenezz33@gmail.com>
2024-09-23 10:22:17 -0600

    Add files via upload

--- Persona.cs
+++ Persona.cs
@@ -8,5 +8,20 @@ namespace Corte1
 {
     internal class Persona
     {
+        public string nombres { get; set; }
+        public string apellidos { get; set; }
+        public DateTime fechadenacimiento { get; set; }
+        public Persona(string nombres, string apellidos, DateTime fechadenacimiento)
+        {
+            Nombres = nombres;
+            Apellidos = apellidos;
+            Fechadenacimiento = fechadenacimiento;
+        }
+        public override string ToString()
+        {
+            return $"Nmbres} {apellidos}, Fecha De Nacimiento: {fechadenacimiento.ToShortDateString};
     }
-}
+
+
+
+    } }
