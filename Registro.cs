8bd72e6acb2cc5edfe5182195a29f1f42cb0928f
HarveyMGJ <hgonzalezjimenezz33@gmail.com>
2024-09-23 10:44:26 -0600

    Add files via upload

--- Registro.cs
+++ Registro.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,28 @@ namespace Corte1
 {
     internal class Registro
     {
+        public class Registro
+        {
+            private Persona[] personas;
+            private int contador;
+            public Registro()
+            {
+                personas = new Persona[30];
+                contador = 0;
+            }
+            public void AgregarPersona(Persona persona)
+            {
+                if (contador < 0)
+                {
+                    persona[contador] = persona;
+                    contador++;
+                    Console.WriteLine("Persona agregada correctamante");
+                }
+                {
+                    else
+                }
+                Console.WriteLine("El registro está lleno, no se puede agregar más personas");
+             }
+        }
     }
 }
